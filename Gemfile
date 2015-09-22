ef12c19c26d4667df4e6ba53054c0b6eb2f131dc
Alex Lemons <alexlemons1@gmail.com>
2015-09-22 11:31:24 +0100

    link.rb created

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+
+group :test do
+  gem 'rspec'
+  gem 'capybara'
+end
---
+++ spec/features/viewing_links_spec.rb
@@ -0,0 +1,16 @@
+feature 'Viewing Links' do
+
+  scenario 'I can see existing links on the links page' do
+       Link.new(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+
+       visit '/links'
+
+       expect(page.status_code).to eql(200)
+
+      within 'ul#links' do
+        expect(page).to have_content('Makers Academy')
+      end
+
+
+  end
+end
