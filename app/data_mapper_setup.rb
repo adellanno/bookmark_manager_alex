fc9d6f1af12d08051e55f07a47877251bf37cb70
adellanno <anthony@dellanno.co.uk>
2015-09-23 12:21:33 +0100

    added DATABASE to heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,7 +3,7 @@ require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, "postgres://ufblrttxffajsi:RahFUPTkK8waLyrfubQcfNo0GQ@ec2-54-243-149-147.compute-1.amazonaws.com:5432/d1dfd0sdhloksh")
 
 require './app/models/link'
 
--- config.ru
+++ config.ru
@@ -2,5 +2,4 @@ require 'rubygems'
 require File.join(File.dirname(__FILE__), 'app/app.rb')
 
 run Sinatra::Application
-
 run Controller
