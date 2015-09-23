16350eb25d36447d47bd516f5e360a9943be7353
adellanno <anthony@dellanno.co.uk>
2015-09-23 14:04:50 +0100

    test database and heroku database separated

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,9 +1,15 @@
 require 'data_mapper'
 
 
-env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://ufblrttxffajsi:RahFUPTkK8waLyrfubQcfNo0GQ@ec2-54-243-149-147.compute-1.amazonaws.com:5432/d1dfd0sdhloksh")
+
+if env = ENV['RACK_ENV'] == 'test'
+  database = "postgres://localhost/bookmark_manager_test"
+else
+  database = "postgres://xlfxowtaqrqtqw:UhZZJT6JnX_KmMyfXiX8s5MAL5@ec2-54-243-149-147.compute-1.amazonaws.com:5432/d4ko46470jier7"
+end
+
+DataMapper.setup(:default, database)
 
 require './app/models/link'
 
